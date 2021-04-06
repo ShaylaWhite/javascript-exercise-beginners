5231e3d3b0d55542819f1b5cfde1e17e298ec892
ShaylaWhite <shayladwhite1@gmail.com>
2021-04-05 21:05:19 -0700

    completed practice problem

--- Practice.js
+++ Practice.js
@@ -9,5 +9,10 @@ function getYears(years) {
 }
 
 
-/* This is
-a comment */
+
+const years = [1763, 1972, 1925, 1916, 1984, 1124, 1950, 2020]
+
+function getYears(years) {
+	return years.filter(year => year > 1950)
+  
+}
